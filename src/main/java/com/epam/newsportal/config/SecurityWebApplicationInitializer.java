0b9e2f5544c3c44762f52d7995face0a320516c0
Maria <maria@MBP-Maria.lan>
2022-04-19 15:31:09 +0600

    XML configuration files added

--- src/main/java/com/epam/newsportal/config/SecurityWebApplicationInitializer.java
+++
@@ -1,7 +0,0 @@
-package com.epam.newsportal.config;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
-
-}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,11 @@
+<html>
+<body>
+
+<h3>Spring News Portal REST Demo</h3>
+
+<hr>
+
+<a href="${pageContext.request.contextPath}/api/news">Get All News</a>
+
+</body>
+</html>
